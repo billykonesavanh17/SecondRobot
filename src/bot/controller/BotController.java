e02a0d017d9d9ca7c7c1436dbfc47d07d0cf9a59
Billy Konesavanh <billykonesavanh17@gmail.com>
2016-03-09 08:29:09 -0700

    Added drive to bot

--- src/bot/controller/BotController.java
+++ src/bot/controller/BotController.java
@@ -4,10 +4,15 @@ import bot.model.EV3Bot;
 
 public class BotController
 {
-
+	private EV3Bot billyBot;
+	
+	private BotController()
+	{
+		billyBot = new EV3Bot();
+	}
 	
 	public void start()
 	{
-
+		billyBot.driveRoom();
 	}
 }
